cf689658e8353aea75ae310bdf7fe20eedfc7bd6
Parsa <parsaj1381@gmail.com>
2025-01-22 02:03:16 +0330

    add NPC

--- character/utils/movement_component/character_movement_component.gd
+++ character/utils/movement_component/character_movement_component.gd
@@ -2,8 +2,8 @@ class_name CharacterMovementComponent
 extends CharacterComponent
 
 @export var speed: float = 60.0
-@export var roll_multiplier = 1.5
-var roll_duration = 0.6  # Duration in seconds
+@export var roll_multiplier = 1.8
+var roll_duration = 0.6 
 var roll_timer = 0.0
 var is_rolling = false
 var _direction
@@ -15,19 +15,13 @@ func _physics_process(_delta: float) -> void:
 	if is_rolling and entity:
 		roll_timer += _delta
 		
-		# Calculate how far through the roll we are (0.0 to 1.0)
 		var roll_progress = roll_timer / roll_duration
 		
 		if roll_progress >= 1.0:
-			# Roll has finished
 			is_rolling = false
 			roll_timer = 0.0
 		else:
-			# Create a smooth movement curve that starts fast and slows down
-			# Using a sine curve for smooth deceleration
-			var speed_factor = cos(roll_progress * PI * 0.5)  # Goes from 1 to 0 smoothly
-			
-			# Calculate velocity based on direction, speed, and multiplier
+			var speed_factor = cos(roll_progress * PI * 0.5)  
 			entity.velocity = _direction * speed * roll_multiplier * speed_factor
 			entity.move_and_slide()
 
